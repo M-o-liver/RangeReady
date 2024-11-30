e9cfc1894e3b9eca65623a6ff174d40963662d5c
Neeham Khalid <Neehamk@gmail.com>
2024-11-30 00:35:50 -0500

    Debug .env variable

--- api/db.php
+++ api/db.php
@@ -2,6 +2,12 @@
 require_once __DIR__ . '/vendor/autoload.php';
 Dotenv\Dotenv::createImmutable(__DIR__)->load();
 
+// Debugging to see if the values are loaded correctly
+echo 'DB Host: ' . $_ENV['DB_HOST'] . '<br>';
+echo 'DB Username: ' . $_ENV['DB_USERNAME'] . '<br>';
+echo 'DB Password: ' . $_ENV['DB_PASSWORD'] . '<br>';
+echo 'DB Database: ' . $_ENV['DB_DATABASE'] . '<br>';
+
 // Database credentials from .env file
 $dbHost = $_ENV['DB_HOST'];
 $dbUsername = $_ENV['DB_USERNAME'];
