6d758191ddfdf0d47b74bf3f72099928d53d59cb
Neeham Khalid <Neehamk@gmail.com>
2024-11-30 22:14:01 -0500

    UI - Let's use react-table

--- src/components/StartActivity/StartActivity.js
+++ src/components/StartActivity/StartActivity.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import './StartActivity.css';
 
 function StartActivity() {
   const [rows, setRows] = useState([{ sn: "", name: "", email: "", unit: "" }]);
